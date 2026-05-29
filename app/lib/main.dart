84d0e813adfeddc0eca869927fb852371640df70
Thang Nguyen <minhthang0403@gmail.com>
2026-05-29 09:46:27 +0700

    feat: register SyncProvider/SyncService, wire focus pull and mutation push

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,8 +10,11 @@ import 'providers/settings_provider.dart';
 import 'providers/snippet_provider.dart';
 import 'providers/local_session_provider.dart';
 import 'providers/terminal_layout_provider.dart';
+import 'providers/sync_provider.dart';
 import 'services/ssh_service.dart';
 import 'services/storage_service.dart';
+import 'services/supabase_service.dart';
+import 'services/sync_service.dart';
 import 'screens/main_screen.dart';
 import 'theme/app_theme.dart';
 
@@ -19,6 +22,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
   await windowManager.setMinimumSize(const Size(800, 600));
+  await SupabaseService.initialize();
   runApp(const YourSSHApp());
 }
 
@@ -29,17 +33,20 @@ class YourSSHApp extends StatefulWidget {
   State<YourSSHApp> createState() => _YourSSHAppState();
 }
 
-class _YourSSHAppState extends State<YourSSHApp> {
+class _YourSSHAppState extends State<YourSSHApp> with WindowListener {
   late final StorageService _storage;
   late final SshService _ssh;
   late final HostProvider _hostProvider;
   late final KeyProvider _keyProvider;
   late final SettingsProvider _settingsProvider;
   late final SessionProvider _sessionProvider;
+  late final SyncProvider _syncProvider;
+  late final SyncService _syncService;
 
   @override
   void initState() {
     super.initState();
+    windowManager.addListener(this);
     _storage = StorageService();
     _ssh = SshService(_storage);
     _hostProvider = HostProvider(_storage);
@@ -50,14 +57,49 @@ class _YourSSHAppState extends State<YourSSHApp> {
     _sessionProvider.autoReconnectEnabled = () => _settingsProvider.autoReconnect;
     _sessionProvider.reconnectAttempts = () => _settingsProvider.reconnectAttempts;
     _sessionProvider.tmuxEnabled = () => _settingsProvider.tmuxEnabled;
+    _syncProvider = SyncProvider();
+    _syncService = SyncService(_syncProvider, SupabaseService());
+
+    _hostProvider.onMutation = () => _syncService.push(
+          hosts: _hostProvider.allHosts,
+          loadPasswords: _loadAllPasswords,
+        );
+
+    _syncService.startRetryTimer(
+      getHosts: () async => _hostProvider.allHosts,
+      loadPasswords: _loadAllPasswords,
+    );
+  }
+
+  Future<Map<String, String>> _loadAllPasswords() async {
+    final passwords = <String, String>{};
+    for (final host in _hostProvider.allHosts) {
+      final pw = await _storage.loadPassword(host.id);
+      if (pw != null) passwords['pw_${host.id}'] = pw;
+    }
+    return passwords;
+  }
+
+  @override
+  void onWindowFocus() {
+    if (_syncProvider.enabled) {
+      _syncService.pull().then((payload) {
+        if (payload != null) {
+          _hostProvider.replaceAll(payload.hosts, payload.passwords);
+        }
+      });
+    }
   }
 
   @override
   void dispose() {
+    windowManager.removeListener(this);
+    _syncService.dispose();
     _hostProvider.dispose();
     _keyProvider.dispose();
     _settingsProvider.dispose();
     _sessionProvider.dispose();
+    _syncProvider.dispose();
     super.dispose();
   }
 
@@ -65,11 +107,14 @@ class _YourSSHAppState extends State<YourSSHApp> {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        Provider.value(value: _storage),
         Provider.value(value: _ssh),
         ChangeNotifierProvider.value(value: _hostProvider),
         ChangeNotifierProvider.value(value: _keyProvider),
         ChangeNotifierProvider.value(value: _settingsProvider),
         ChangeNotifierProvider.value(value: _sessionProvider),
+        ChangeNotifierProvider.value(value: _syncProvider),
+        Provider.value(value: _syncService),
         ChangeNotifierProvider(create: (_) => PortForwardProvider()),
         ChangeNotifierProvider(create: (_) => SnippetProvider()),
         ChangeNotifierProvider(create: (_) {
