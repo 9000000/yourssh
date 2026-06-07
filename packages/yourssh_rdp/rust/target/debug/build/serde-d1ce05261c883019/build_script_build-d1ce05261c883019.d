4867e81675337a8a1ba0ad7e8654453c78461c83
Thang Nguyen <minhthang0403@gmail.com>
2026-06-07 13:26:07 +0700

    chore: gitignore yourssh_rdp rust target dir

--- packages/yourssh_rdp/rust/target/debug/build/serde-d1ce05261c883019/build_script_build-d1ce05261c883019.d
+++
@@ -1,5 +0,0 @@
-/Users/thangnguyen/Projects/Personal/yourssh/packages/yourssh_rdp/rust/target/debug/build/serde-d1ce05261c883019/build_script_build-d1ce05261c883019.d: /Users/thangnguyen/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/Users/thangnguyen/Projects/Personal/yourssh/packages/yourssh_rdp/rust/target/debug/build/serde-d1ce05261c883019/build_script_build-d1ce05261c883019: /Users/thangnguyen/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/Users/thangnguyen/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs:
